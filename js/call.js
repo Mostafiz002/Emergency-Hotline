7c2a1f36f6af91b41a5de4a485182bef53457e21
Mostafiz002 <mo.mahin4@gmail.com>
2025-08-27 19:14:21 +0600

    added call history functionality

--- js/call.js
+++ js/call.js
@@ -3,21 +3,46 @@ let availableBalance = parseInt(
 );
 
 const services = document.getElementsByClassName("services");
+const serviceTitles = document.getElementsByClassName("service-title");
 const numbers = document.getElementsByClassName("number");
 const calls = document.getElementsByClassName("call-btn");
 
 for (let i = 0; i < calls.length; i++) {
   calls[i].addEventListener("click", function () {
+    const serviceTitle = serviceTitles[i].innerText;
     const service = services[i].innerText;
     const num = numbers[i].innerText;
-    
+    const time = new Date().toLocaleTimeString();
+
     if (availableBalance > 0) {
       availableBalance -= 20;
       document.getElementById("available-balance").innerText = availableBalance;
 
       alert("📞 Calling " + service + " " + num + "...");
+
+      const historyContainer = document.getElementById("call-history-cont");
+      const div = document.createElement("div");
+      div.innerHTML = `
+      <div
+              class="mt-3 p-4 bg-[#fafafa] flex justify-between items-center rounded-lg gap-2"
+            >
+              <div>
+                <h4 class="text-base font-semibold text-black">
+                  ${serviceTitle}
+                </h4>
+                <p class="text-gray-600 text-sm">${num}</p>
+              </div>
+              <div>
+                <p class="text-sm font-normal text-black">${time}</p>
+              </div>
+            </div>
+      `;
+
+      historyContainer.appendChild(div);
     } else {
-      alert("❌ You don’t have enough balance. You need at least 20 coins to make a call.");
+      alert(
+        "❌ You don’t have enough balance. You need at least 20 coins to make a call."
+      );
     }
   });
 }
