4e5fde40300b2f8d801263e3c759a17c26ec0009
Mostafiz002 <mo.mahin4@gmail.com>
2025-08-27 20:19:16 +0600

    added copy button functionality

---
+++ js/copy.js
@@ -0,0 +1,15 @@
+const copyCount = document.getElementById("copy-count");
+let totalCopy = 0;
+
+const copyButtons = document.getElementsByClassName("copy-buttons");
+
+for (let i = 0; i < copyButtons.length; i++) {
+  copyButtons[i].addEventListener("click", function () {
+    const num = numbers[i].innerText;
+    navigator.clipboard.writeText(num);
+    alert("Number has been copied: " + num);
+
+    totalCopy++;
+    copyCount.innerText = totalCopy;
+  });
+}
